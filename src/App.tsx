31fb31fdf095bd95ff32e1bac63525aa2674f831
sowebb <stark247@gmail.com>
2025-09-29 18:12:28 -0500

    Refactor App component to use AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import WeatherPage from "./pages/WeatherPage";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
-import TermsPage from "./pages/TermsPage";
 import { ThemeProvider } from "./theme/ThemeProvider";
+import AppRoutes from "./AppRoutes";
 
-function App() {
+export default function App() {
   return (
     <ThemeProvider>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<WeatherPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/terms" element={<TermsPage />} />
-        </Routes>
+        <AppRoutes />
       </Router>
     </ThemeProvider>
   );
 }
-
-export default App;
